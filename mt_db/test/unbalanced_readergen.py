ccec3b8b16a3cb688aaf3ff2922da2bca0599e6f
gerow <gerow.mike@gmail.com>
2013-02-21 18:24:32 -0800

    We finally have a test that show improvement in all parts!

---
+++ mt_db/test/unbalanced_readergen.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import random
+
+def choose_and_remove( items ):
+  # pick an item index
+  if items:
+    index = random.randrange( len(items) )
+    return items.pop(index)
+  return None
+
+def main():
+  keys = []
+  for i in range(2000000):
+    keys.append(str(i))
+
+  for i in range(5000):
+    print "q " + keys[(random.randrange(len(keys)))]
+
+if __name__ == "__main__":
+  main()
